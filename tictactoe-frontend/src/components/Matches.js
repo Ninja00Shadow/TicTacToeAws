caee05473cc693eb7a32225be05a237418ea337d
Dawid <mrshadow813@gmail.com>
2024-06-08 22:14:23 +0200

    matches button

--- tictactoe-frontend/src/components/Matches.js
+++ tictactoe-frontend/src/components/Matches.js
@@ -1,16 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './Matches.css';
+import { useNavigate } from 'react-router-dom';
 
 const Matches = () => {
   const [matches, setMatches] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchMatches = async () => {
       try {
-        const response = await axios.get('http://44.205.169.11:8000/matches'); // Zmień URL na odpowiedni endpoint API
+        const response = await axios.get('http://44.205.169.11:8000/matches');
         setMatches(response.data.matches);
       } catch (error) {
         setError('Error fetching matches');
@@ -32,6 +34,7 @@ const Matches = () => {
 
   return (
     <div className="matches-container">
+      <button className="back-button" onClick={() => navigate('/')}>Back</button>
       {matches.map((match, index) => (
         <div key={index} className="match">
           <div className={`player ${match.winner === match.player1 ? 'winner' : ''}`}>
